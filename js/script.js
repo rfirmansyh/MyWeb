8f62ddd7990a139d375f2540b2da82e6699bf75e
rfirmansyh <fsyah7052@gmail.com>
2019-12-08 13:49:14 +0700

    done

--- js/script.js
+++ js/script.js
@@ -4,60 +4,15 @@ $(function () {
 
   const bMenu = $('.menu-button');
   const menu = $('.menu-navbar');
-  const webProducts = 6;
-  const tabletProducts = 3;
-  const mobileProducts = 1;
 
-  // function showProductsMobile() {
-  //   let contentText = "";
-  //   for (let i = 0; i < mobileProducts; i++) {
-  //     contentText += '<div class="product-content on-product"><img class="content-image on-product"src="img/sengon.jpg" alt=""><h1 class="name on-product">Kemitraan Bibit Sengon</h1><hr><p class="address on-product">Lahan Kelompok Tani</p><p class="price on-product">Harga : <span class="go-right">Rp 4.000.0000</span></p><p class="advantages on-product">Keuntungan : <span class="go-right">17% per tahun</span></p><button class="more-button on-product">Selengkapnya</button></div>'
-  //   }
-  //   return contentText;
-  // }
-  // function showProductsTablet() {
-  //   let contentText = "";
-  //   for (let i = 0; i < tabletProducts; i++) {
-  //     contentText += '<div class="product-content on-product"><img class="content-image on-product"src="img/sengon.jpg" alt=""><h1 class="name on-product">Kemitraan Bibit Sengon</h1><hr><p class="address on-product">Lahan Kelompok Tani</p><p class="price on-product">Harga : <span class="go-right">Rp 4.000.0000</span></p><p class="advantages on-product">Keuntungan : <span class="go-right">17% per tahun</span></p><button class="more-button on-product">Selengkapnya</button></div>'
-  //   }
-  //   return contentText;
-  // }
-  // function showProductsTablet2() {
-  //   let contentText = "";
-  //   for (let i = 0; i < 2; i++) {
-  //     contentText += '<div class="product-content on-product"><img class="content-image on-product"src="img/sengon.jpg" alt=""><h1 class="name on-product">Kemitraan Bibit Sengon</h1><hr><p class="address on-product">Lahan Kelompok Tani</p><p class="price on-product">Harga : <span class="go-right">Rp 4.000.0000</span></p><p class="advantages on-product">Keuntungan : <span class="go-right">17% per tahun</span></p><button class="more-button on-product">Selengkapnya</button></div>'
-  //   }
-  //   return contentText;
-  // }
-  // function showProductsDesktop() {
-  //   let contentText = "";
-  //   for (let i = 0; i < webProducts; i++) {
-  //     contentText += '<div class="product-content on-product"><img class="content-image on-product"src="img/sengon.jpg" alt=""><h1 class="name on-product">Kemitraan Bibit Sengon</h1><hr><p class="address on-product">Lahan Kelompok Tani</p><p class="price on-product">Harga : <span class="go-right">Rp 4.000.0000</span></p><p class="advantages on-product">Keuntungan : <span class="go-right">17% per tahun</span></p><button class="more-button on-product">Selengkapnya</button></div>'
-  //   }
-  //   return contentText;
-  // }
-
-  // function initiateProduct() {
-  //   let width = $('body').outerWidth();
-  //   if ( width > 900 ) {
-  //     $('.product-body .cp').html(showProductsDesktop());
-  //     $('.jumbotron-full').css('z-index', '1');
-  //     menu.css('height', 'inherit');
-  //   } else if ( width > 550 ) {
-  //     $('.product-body .cp').html(showProductsTablet());
-  //     $('.jumbotron-full').css('z-index', '1');
-  //     menu.css('height', '0px');
-  //   } else if ( width > 400) {
-  //     $('.product-body .cp').html(showProductsTablet2());
-  //     $('.jumbotron-full').css('z-index', '1');
-  //     menu.css('height', '0px');
-  //   }  else {
-  //     $('.product-body .cp').html(showProductsMobile());
-  //     $('.jumbotron-full').css('z-index', '1');
-  //     menu.css('height', '0px');
-  //   }
-  // }
-
+  $('.page-scroll').on('click', function (e) {
+    let dec = $(this).attr('href');
+    let getEl = $(dec);
+    $('html,body').animate({
+      scrollTop: getEl.offset().top - 100
+    }, 1000);
+    e.preventDefault();
+  })
 
   function getWidthSize() {
 
@@ -82,8 +37,6 @@ $(function () {
   $(window).resize(function () {
     getWidthSize();
   });
-
-
 
   bMenu.on('click', function () {
 
@@ -128,21 +81,11 @@ $(function () {
 
   }
 
+
+
   $('.login-form').css('z-index', '-20');
   $('.login-form').css('opacity', '0');
 
-  $('.register').on('click', function (e) {
-    if ($('.login-form').css('z-index', '-20') && $('.login-form').css('opacity', '0')) {
-      $('.login-form').css('z-index', '100');
-      $('.login-form').css('opacity', '1');
-      e.preventDefault();
-    }
-  });
-
-  $('.close').on('click', function () {
-    $('.login-form').css('z-index', '-20');
-    $('.login-form').css('opacity', '0');
-  });
 
   $('#next1').on('click', clickNext);
 
@@ -156,15 +99,78 @@ $(function () {
     $('.select-desc').addClass('row-select-active');
     $('.select-advantages').removeClass('row-select-active');
     $('.corp-body-detail').html('<p>Bibit Sengon adalah komoditas penting untuk Indonesia. Industri pakan ternak merupakan salah satu konsumen terbesar jagung nasional dengan tingkat kebutuhan sekitar 12 juta ton per tahun dan akan  terus meningkat. Untuk mencukupi kebutuhan tersebut, kita membutuhkan benih jagung sekitar 60,000 ton,dimana saat ini produksi benih jagung nasional baru berada di kisaran 30,000 ton per tahun. Sebagian besarnya masih diproduksi oleh perusahaan global produsen benih. <br>Oleh karena itu masih sangat besar ruang bagi produsen lokal untuk masuk. Terlebih sudah ada bibit jagung berkualitas dari beberapa Balai Penelitian Serelia milik Indonesia. Program kemitraan penangkaran bibit jagung yang akan dilakukan oleh Kelompok Tani Subur di Sanankerto Malang ini akan memberdayakan sekitar 30 orang lebih petani jagung dengan luas garapan 40 hektar.</p>');
+    window.location.href = '#detail';
   });
 
   $('.select-advantages').on('click', function () {
     $('.select-advantages').addClass('row-select-active');
     $('.select-desc').removeClass('row-select-active');
     $('.corp-body-detail').html('<div class="table-advan"><div class="row-table-advan"><div class="th-table-advan">Tahun Ke</div><div class="th-table-advan">Hasil Bagi Bersih</div></div><div class="row-table-advan gray"><div class="td-table-advan">1</div><div class="td-table-advan">Rp. 720.000</div></div><div class="row-table-advan "><div class="td-table-advan">2</div><div class="td-table-advan">Rp. 720.000</div></div><div class="row-table-advan gray"><div class="td-table-advan">3</div><div class="td-table-advan">Rp. 720.000</div></div><div class="row-table-advan "><div class="td-table-advan" >Pengembalian Modal</div><div class="td-table-advan">Rp. 4.000.000</div></div><div class="row-table-advan gray"><div class="td-table-advan">Total</div><div class="td-table-advan">Rp. 6.160.000</div></div></div>');
+    window.location.href = '#detail';
+  });
+
+  // jsoning
+  $(document).ready(function () {
+    $.get(
+      "http://127.0.0.1:5500/ajax/data.txt", function (data) {
+        $('#list-product').html(data);
+      });
 
   });
+
+  // $("#submit-login").on('click', function (e) {
+  //   e.preventDefault();
+  //   let email = "";
+  //   let password = "";
+  //   $.get("http://127.0.0.1:5500/ajax/login.json", function (data) {
+  //     console.log(data.user[0]);
+
+  //   });
+  // })
+
+  $("#form-login").on('submit', function (e) {
+    e.preventDefault();
+    let email = $('#email-input').val();
+    let pass = $('#password-input').val();
+
+    $.get("http://127.0.0.1:5500/ajax/login.json", function (data) {
+      for (let i = 0; i < data.user.length; i++) {
+        if (email == data.user[i].email && pass == data.user[i].password) {
+          document.location.href = 'http://127.0.0.1:5500/dashboard.html';
+        } else {
+          $('.login-indicator').css('display', 'block');
+        }
+      }
+    });
+  })
+
 
 });
 
 
+
+// no jquery block
+function login() {
+  $('#register').css('z-index', '-20');
+  $('#register').css('opacity', '0');
+  $('#login').css('z-index', '100');
+  $('#login').css('opacity', '1');
+}
+function register() {
+  $('#login').css('z-index', '-20');
+  $('#login').css('opacity', '0');
+  $('#register').css('z-index', '100');
+  $('#register').css('opacity', '1');
+}
+function logout() {
+  $('#login').css('z-index', '-20');
+  $('#login').css('opacity', '0');
+  $('#register').css('z-index', '-20');
+  $('#register').css('opacity', '0');
+  $('#logout').css('z-index', '100');
+  $('#logout').css('opacity', '1');
+}
+function closeModal() {
+  $('.login-form').css('z-index', '-20');
+  $('.login-form').css('opacity', '0');
+}
